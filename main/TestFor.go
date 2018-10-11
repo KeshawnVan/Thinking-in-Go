a58ec7a53d17605456183a1cc0142862bb71a22f
Fankx <fankx@startimes.com.cn>
2018-10-11 11:00:38 +0800

    basic

---
+++ main/TestFor.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	for i := 0; i < 5; i++ {
+		println(i)
+	}
+	for i := 10; i > 0; i-- {
+		println(i)
+	}
+
+	x := 0
+	for x < 5 {
+		println(x)
+		x++
+	}
+
+	for {
+		println(x)
+		x--
+		if x < 0 {
+			break
+		}
+	}
+}
