e474195f5d8e4b119508f03b3b872a078dcae63a
yuming <kaixiang.fkx@alibaba-inc.com>
2019-09-19 20:41:26 +0800

    point

--- main/TestPoint.go
+++ main/TestPoint.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type info struct {
 	x int
 }
@@ -21,17 +23,38 @@ func main() {
 	pa.x += 100
 	println(pa.x)
 
+	fmt.Printf("a address is %p \n", &a)
 	modifyInfo(a)
 	println(a.x)
 
 	modifyInfoByPoint(&a)
 	println(a.x)
+
+	i := 100
+	fmt.Printf("i address is %d \n", &i)
+	modifyInt(i)
+	fmt.Printf("i address is %d \n", &i)
+	modifyIntByPoint(&i)
+	fmt.Printf("i address is %d \n", &i)
+	fmt.Printf("i is %d", i)
 }
 
 func modifyInfo(i info) {
+	fmt.Printf("a address is %p \n", &i)
 	i.x = 1000
 }
 
 func modifyInfoByPoint(pi *info) {
+	fmt.Printf("a address is %p \n", pi)
 	pi.x = 1000
+}
+
+func modifyInt(i int) {
+	fmt.Printf("i address is %d \n", &i)
+	i += 100
+}
+
+func modifyIntByPoint(pi *int) {
+	fmt.Printf("i address is %d \n", pi)
+	*pi += 100
 }
